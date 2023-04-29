6795207475ab2499e0c7152efe74750bca01b443
athensclub <athens_club@hotmail.com>
2023-04-29 17:38:25 +0700

    edit: button highlighting on each view

--- frontend/index.js
+++ frontend/index.js
@@ -9,13 +9,19 @@ const TOTAL_SCORE_VIEW = "TOTAL_SCORE";
 const setView = (view) => {
   if (view === MY_SCORE_VIEW) {
     myScoreView.classList.remove("hidden");
+    myScoreButton.style.borderWidth = "2px";
     totalScoreView.classList.add("hidden");
+    totalScoreButton.style.borderWidth = "0";
   } else if (view === TOTAL_SCORE_VIEW) {
     myScoreView.classList.add("hidden");
+    myScoreButton.style.borderWidth = "0";
     totalScoreView.classList.remove("hidden");
+    totalScoreButton.style.borderWidth = "2px";
   } else {
     throw Error("Unknown view: " + view);
   }
 };
 myScoreButton.onclick = () => setView(MY_SCORE_VIEW);
 totalScoreButton.onclick = () => setView(TOTAL_SCORE_VIEW);
+// initial view is my score
+setView(MY_SCORE_VIEW);
