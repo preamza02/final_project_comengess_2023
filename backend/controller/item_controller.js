ff656a5f112f5ec7c4e94d4dbc5f025ca7f24f7c
Supharoek Kongprapane <pream5138@gmail.com>
2023-05-02 01:13:13 +0700

    add item_controller

--- backend/controller/item_controller.js
+++ backend/controller/item_controller.js
@@ -1,55 +1,44 @@
-const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const {
-  PutCommand,
-  DeleteCommand,
-  ScanCommand,
-} = require("@aws-sdk/lib-dynamodb");
-const docClient = new DynamoDBClient({ regions: "us-east-1" });
+function get_course_info(cv_cid) {}
+function get_greaded_item(cv_cid) {}
+function get_all_course() {}
 
-exports.getCourses = async (req, res) => {
-  const params = {
-    TableName: "course",
-  };
-  try {
-    const data = await docClient.send(new ScanCommand(params));
-    res.send(data.Items);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send(err);
+function getPayload() {
+  all_course = get_all_course();
+  const payload = [];
+  all_course = all_course["data"]["student"];
+  for (let i = 0; i < all_course.length; i++) {
+    const cv_cid = all_course[i]["cv_cid"];
+    const course_no = all_course[i]["course_no"];
+    const year = all_course[i]["year"];
+    var info = get_course_info(cv_cid);
+
+    info = info["data"];
+    const name = title["title"];
+    const course_icon = title["course_icon"];
+
+    var gi = get_greaded_item(cv_cid);
+    gi = gi["data"];
+    var item_list = [];
+    for (let j = 0; j < all_course.length; j++) {
+      item_list.put({
+        name: gi[j]["title"],
+        score: 0,
+        max_score: gi[j]["raw_total"],
+        percent: gi[j]["weight_in_group"],
+      });
+    }
+    payload.put({
+      cv_cid: cv_cid,
+      course_no: course_no,
+      year: year,
+      name: name,
+      is_selected: false,
+      grade: 0.0,
+      credit: 3.0,
+      course_icon: course_icon,
+      item_list: item_list,
+    });
   }
-};
-exports.getCoursesID = async (req, res) => {
-  const params = {
-    TableName: "course",
-    IndexName: "student_id",
-    KeyConditionExpression: "student_id = :id",
-    ExpressionAttributeValues: {
-      ":id": req.params.id,
-    },
-  };
-  try {
-    const data = await docClient.send(new ScanCommand(params));
-    res.send(data.Items);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send(err);
-  }
-};
-exports.addItem = async (req, res) => {
-  const item = {
-    student_id: req.params.id,
-    isselect: false,
-    score: [],
-  };
-  const params = {
-    Item: item,
-    TableName: "course",
-  };
-  try {
-    const data = await docClient.send(new PutCommand(params));
-    res.send(item);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send(err);
-  }
-};
+  return payload;
+}
+function startingCourse() {}
--- backend/router/item_router.js
+++ backend/router/item_router.js
@@ -3,10 +3,7 @@ const itemController = require("../controller/item_controller");
 
 const router = express.Router();
 
-// router.get("/get_course/:id", itemController.getCourses);
-router.get("/get_course/", itemController.getCourses);
-router.get("/get_course/:id", itemController.getCoursesID);
-router.get("/put_score/:id", itemController.addItem);
-router.get("/put_score/:id", itemController.addItem);
+router.get("/get_course/:student_id", itemController.getCourses);
+router.post("/put_course/:student_id", itemController.postCourses);
 
 module.exports = router;
